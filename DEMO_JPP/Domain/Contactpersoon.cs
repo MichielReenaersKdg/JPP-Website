3af4ab385a365d54fff0f6d3e688831f1044a8ea
Mouzza <moussa.elb@hotmail.be>
2015-03-31 10:47:43 +0200

    Reset

---
+++ DEMO_JPP/Domain/Contactpersoon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JPP.Domain
+{
+    public class Contactpersoon
+    {
+        public int id { get; set; }
+        public string naam { get; set; }
+        public string straat { get; set; }
+        public string gemeente { get; set; }
+        public string email { get; set; }
+        public string telefoonNr { get; set; }
+        public string ipAddress { get; set; }
+
+
+    }
+}
