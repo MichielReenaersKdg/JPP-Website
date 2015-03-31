1d8a0076c41ef8623a5a411eccab33cfaf86eae9
Mouzza <moussa.elb@hotmail.be>
2015-03-31 10:28:14 +0200

    Changes
    Changes

---
+++ DEMO_JPP/BL/IContactManager.cs
@@ -0,0 +1,16 @@
+﻿using JPP.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JPP.BL
+{
+    interface IContactManager
+    {
+        IEnumerable<Contactpersoon> GetContactpersonen();
+        void RemoveContactpersoon(int id);
+        Contactpersoon GetContactpersoon(int id);
+    }
+}
---
+++ DEMO_JPP/DAL/IContactRepository.cs
@@ -0,0 +1,17 @@
+﻿using JPP.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JPP.DAL
+{
+    public interface IContactRepository
+    {
+
+        IEnumerable<Contactpersoon> ReadContactpersonen();
+        Contactpersoon ReadContactpersoon(int id);
+        void DeleteContactpersoon(int id);
+    }
+}
